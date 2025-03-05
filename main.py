8d6f753ec43da52ef8bb75f757e4e60d8d3061f1
Natasa Kukic <natasa.kukic77@gmail.com>
2025-03-05 11:45:16 +0100

    delRemoving display and total functions

--- main.py
+++ main.py
@@ -16,9 +16,6 @@ manager.add_prod(product1)
 manager.add_prod(product2)
 manager.add_prod(product5)
 
-manager.display_all_prod()
-
-print(f"Total price of all products: {manager.total_price()}$")
 
 manager.remove_product_by_name("Canvas")
 print(manager.list_products())
